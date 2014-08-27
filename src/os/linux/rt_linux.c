97bfa6090a916faa7dbb0bbd2b99034971bfef84
chenyuyou <chenyuyou@users.noreply.github.com>
2014-08-27 11:38:28 +0800

    Update rt_linux.c

--- src/os/linux/rt_linux.c
+++ src/os/linux/rt_linux.c
@@ -1185,8 +1185,8 @@ static inline void __RtmpOSFSInfoChange(OS_FS_INFO * pOSFSInfo, BOOLEAN bSet)
 		pOSFSInfo->fsgid = current->fsgid;
 		current->fsuid = current->fsgid = 0;
 #else
-		pOSFSInfo->fsuid = current_fsuid();
-		pOSFSInfo->fsgid = current_fsgid();
+		pOSFSInfo->fsuid = *(int*)&current_fsuid();
+		pOSFSInfo->fsgid = *(int*)&current_fsgid();
 #endif
 		pOSFSInfo->fs = get_fs();
 		set_fs(KERNEL_DS);
